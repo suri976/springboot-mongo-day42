e3a138193b6ec02cf74d192aa74252a3948c451e
SurendraReddy <sweetshiny8@gmail.com>
2022-05-17 12:08:42 +0530

    mongo

---
+++ springboot-mongo-day42/src/main/java/com/jobiak/SpringbootMongoApplication.java
@@ -0,0 +1,27 @@
+package com.jobiak;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@SpringBootApplication
+public class SpringbootMongoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private MongoTemplate mongoTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println(mongoTemplate.getCollectionNames());
+		// TODO Auto-generated method stub
+		
+	}
+
+}
